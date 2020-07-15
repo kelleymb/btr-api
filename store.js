ed0e3fa4badf7226449039c2dc1ea4a0d6adf877
Kelleymb <kelleybuccieri@gmail.com>
2020-07-15 14:07:32 -0400

    store.js added

---
+++ store.js
@@ -0,0 +1,14 @@
+const { applyMiddleware, createStore, } = require('redux')
+const createSagaMiddleware = require('redux-saga')
+const rootReducer = require('./reducers/index')
+const rootSaga = require('./sagas/index')
+const { composeWithDevTools } = require('redux-devtools-extension')
+
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+rootReducer,
+composeWithDevTools(applyMiddleware(sagaMiddleware)));
+sagaMiddleware.run(rootSaga);
+
+export default store;
